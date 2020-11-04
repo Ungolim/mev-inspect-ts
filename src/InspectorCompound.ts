789bfbf8c0384893735797256b7a8b9ffe31b215
fiiiu <alejosalles@gmail.com>
2020-11-04 17:40:34 -0300

    Handling ETH case both as underlying and collateral

--- src/InspectorCompound.ts
+++ src/InspectorCompound.ts
@@ -1,11 +1,11 @@
 import { BigNumber, Contract, providers } from 'ethers'
 import * as _ from "lodash";
-import { Interface } from "ethers/lib/utils";
+import { Interface, TransactionDescription, parseEther } from "ethers/lib/utils";
 
 import { getSigHashes, subcallMatch } from "./utils";
 import { Inspector } from "./Inspector";
-import { ERC20_ABI, COMPOUND_CTOKEN_ABI, COMPOUND_COMPTROLLER_ABI } from "./config/abi";
-import { COMPOUND_COMPTROLLER_ADDRESS, COMPOUND_CETH_ADDRESS } from "./config/addresses";
+import { ERC20_ABI, COMPOUND_CTOKEN_ABI, COMPOUND_CETHER_ABI, COMPOUND_COMPTROLLER_ABI } from "./config/abi";
+import { COMPOUND_COMPTROLLER_ADDRESS, COMPOUND_CETHER_ADDRESS, WETH } from "./config/addresses";
 import {
   ACTION_PROVIDER,
   ACTION_TYPE,
@@ -19,8 +19,10 @@ import { checkServerIdentity } from 'tls';
 
 export class InspectorCompound extends Inspector {
   public cTokenContract: Interface;
-  private static cTokenLiquidationFunctionName = 'liquidateBorrow';
-  private cTokenLiquidationSigs: string;
+  public cEtherContract: Interface;
+  private static liquidationFunctionName = 'liquidateBorrow';
+  private cTokenLiquidationSig: string;
+  private cEtherLiquidationSig: string;
   public cTokenAddresses: Array<string>;
   public underlyingAddresses: Record<string, string>;
 
@@ -28,7 +30,9 @@ export class InspectorCompound extends Inspector {
     super(provider);
 
     this.cTokenContract = new Interface(COMPOUND_CTOKEN_ABI);
-    this.cTokenLiquidationSigs = this.cTokenContract.getSighash(this.cTokenContract.getFunction(InspectorCompound.cTokenLiquidationFunctionName));
+    this.cTokenLiquidationSig = this.cTokenContract.getSighash(this.cTokenContract.getFunction(InspectorCompound.liquidationFunctionName));
+    this.cEtherContract = new Interface(COMPOUND_CETHER_ABI);
+    this.cEtherLiquidationSig = this.cEtherContract.getSighash(this.cEtherContract.getFunction(InspectorCompound.liquidationFunctionName));
     this.cTokenAddresses = cTokenAddresses;
     this.underlyingAddresses = underlyingAddresses;
     // console.log(this.cTokenAddresses);
@@ -39,14 +43,17 @@ export class InspectorCompound extends Inspector {
   static async create(provider: providers.JsonRpcProvider): Promise<InspectorCompound> {
     const comptrollerContract = new Contract(COMPOUND_COMPTROLLER_ADDRESS, COMPOUND_COMPTROLLER_ABI, provider);
     const cTokenAddresses = (await comptrollerContract.getAllMarkets() as Array<string>).map((address) => address.toLowerCase());
-    var underlyingAddresses: Record<string, string> = {}; 
+    let underlyingAddresses: Record<string, string> = {}; 
   
     const cTokenContract = new Interface(COMPOUND_CTOKEN_ABI);
     for (const cTokenAddress of cTokenAddresses){
-      if (cTokenAddress !== COMPOUND_CETH_ADDRESS.toLowerCase()){ // cETH doesn't implement underlying(), handle separately
+      if (cTokenAddress !== COMPOUND_CETHER_ADDRESS.toLowerCase()){ // cEther doesn't implement underlying(), handle separately
         const cTokenContract = new Contract(cTokenAddress, COMPOUND_CTOKEN_ABI, provider);
         const underlyingAddress = await cTokenContract.underlying();
         underlyingAddresses[cTokenAddress.toLowerCase()] = underlyingAddress.toLowerCase();
+      }
+      else {
+        underlyingAddresses[cTokenAddress.toLowerCase()] = WETH.toLowerCase(); // use WETH for tracking cEther underlying, although Compound uses ETH directly
       }
     }
 
@@ -58,32 +65,38 @@ export class InspectorCompound extends Inspector {
     const unknownCalls = _.clone(calls)
 
     const liquidationCalls = _.filter(unknownCalls, (call) =>
-      (this.cTokenAddresses.some((address) => call.action.to === address)) && call.action.input.startsWith(this.cTokenLiquidationSigs))
-      
+      (this.cTokenAddresses.some((address) => call.action.to === address)) && (call.action.input.startsWith(this.cTokenLiquidationSig) || call.action.input.startsWith(this.cEtherLiquidationSig)))
+    
     for (const liquidationCall of liquidationCalls) {
-      const parsedLiquidationCall = this.cTokenContract.parseTransaction({data: liquidationCall.action.input});
+      //////////////////////////////////////
+      // Parse liquidation call and subcalls
+      //
+      let parsedLiquidationCall: TransactionDescription;
+      // cEther case
+      if (liquidationCall.action.to === COMPOUND_CETHER_ADDRESS.toLowerCase()) {
+        parsedLiquidationCall = this.cEtherContract.parseTransaction({data: liquidationCall.action.input});
+      }
+      // cToken case
+      else {
+        parsedLiquidationCall = this.cTokenContract.parseTransaction({data: liquidationCall.action.input});
+      }
         
       const subCallsOfLiquidation = _.remove(unknownCalls, call => {
         return call.transactionHash === liquidationCall.transactionHash &&
         subcallMatch(call, liquidationCall.traceAddress)
       })
-        
-      const collateral: string = parsedLiquidationCall.args.cTokenCollateral.toLowerCase()
-      // console.log(parsedLiquidationCall);
-      // console.log(liquidationCall);
       
-      // TODO: handle ETH case
-      if (liquidationCall.action.to == COMPOUND_CETH_ADDRESS.toLowerCase()){
-        console.log("unhandled cETH liquidation detected in tx: ", liquidationCall.transactionHash);
-        continue
-      }
 
-      const cTokenAddress: string = liquidationCall.action.to ?? ""; // should not be ""
+      ////////////////////////////////////
+      // Recover underlying and collateral
+      //
+      const collateral: string = parsedLiquidationCall.args.cTokenCollateral.toLowerCase()
+      const cTokenAddress: string = liquidationCall.action.to ?? ""; // should never be empty, need check for casting purposes
       const underlying: string = this.underlyingAddresses[cTokenAddress];
-      console.log("Underlying: ", underlying);
-      
       const ZERO = BigNumber.from(0);
-      
+      // console.log("Underlying: ", underlying);
+      // console.log("Collateral: ", collateral);
+            
       const liquidationOffer: LiquidationOffer = {
         sourceToken: underlying, 
         destAmount: ZERO,
@@ -91,7 +104,11 @@ export class InspectorCompound extends Inspector {
         destToken: collateral,
         liquidationDetails: parsedLiquidationCall.args._borrower
       }
-      
+
+
+      ///////////////////////////
+      // Handle reverted calls
+      //
       if (liquidationCall.reverted) {
         result.push({
           provider: ACTION_PROVIDER.COMPOUND,
@@ -104,34 +121,57 @@ export class InspectorCompound extends Inspector {
         })
         continue
       }
-      
+
+
+      ////////////////////////////
       // Parse collateral transfer
+      //
       const cTokenContract = new Interface(COMPOUND_CTOKEN_ABI);
       const collateralCall = _.filter(subCallsOfLiquidation, call => {
         return call.action.to === collateral && call.action.callType === "call"
       });
-      
+    
       const collateralTransferDecode = cTokenContract.parseTransaction({data: collateralCall[1].action.input});
       liquidationOffer.destAmount = collateralTransferDecode.args.seizeTokens;
       // console.log("liquidationOffer.destAmount: ", liquidationOffer.destAmount);
       
-      // Parse underlying transfer
-      const erc20Contract = new Interface(ERC20_ABI);
-      const underlyingCall = _.filter(subCallsOfLiquidation, call => {
-        return call.action.to === underlying && call.action.callType === "call"
-      });
-      
-      const underlyingTransferDecode = erc20Contract.parseTransaction({data: underlyingCall[0].action.input});
-      liquidationOffer.sourceAmount = underlyingTransferDecode.args.value
-      
-      // // these should macth, as the underlying amount is passed as parameter
-      // console.log("liquidationOffer.sourceAmount: ", liquidationOffer.sourceAmount);
-      // console.log("calldata argument repayAmount: ", parsedLiquidationCall.args.repayAmount);
-      
-      // if return code is 0, liquidation was successful, otherwise it failed, label as "CHECKED"
-      const ZERO_STRING = "0x0000000000000000000000000000000000000000000000000000000000000000";
-      const callStatus = liquidationCall.result.output === ZERO_STRING ? ACTION_STATUS.SUCCESS : ACTION_STATUS.CHECKED;
 
+      ////////////////////////////
+      // Parse underlying transfer
+      //
+      let callStatus;
+      
+      // ETH case
+      if (liquidationCall.action.to === COMPOUND_CETHER_ADDRESS.toLowerCase()){
+        liquidationOffer.sourceAmount = BigNumber.from(liquidationCall.action.value);
+
+        // No return value for CETHER liquidations, reverts on error (handled above)
+        callStatus = ACTION_STATUS.SUCCESS;
+      }
+      
+      // ERC20 case
+      else {
+        const erc20Contract = new Interface(ERC20_ABI);
+        const underlyingCall = _.filter(subCallsOfLiquidation, call => {
+          return call.action.to === underlying && call.action.callType === "call"
+        });
+        
+        const underlyingTransferDecode = erc20Contract.parseTransaction({data: underlyingCall[0].action.input});
+        liquidationOffer.sourceAmount = underlyingTransferDecode.args.value
+        
+        // // Sanity check: these should macth, as the underlying amount is passed as parameter
+        // console.log("liquidationOffer.sourceAmount: ", liquidationOffer.sourceAmount);
+        // console.log("calldata argument repayAmount: ", parsedLiquidationCall.args.repayAmount);
+        
+        // If return code is 0, liquidation was successful, otherwise it failed, label as "CHECKED"
+        const ZERO_STRING = "0x0000000000000000000000000000000000000000000000000000000000000000";
+        callStatus = liquidationCall.result.output === ZERO_STRING ? ACTION_STATUS.SUCCESS : ACTION_STATUS.CHECKED;
+      }
+
+      
+      ///////////////////////////
+      // Record LiquidationAction
+      //
       const action: LiquidationAction = {
         provider: ACTION_PROVIDER.COMPOUND,
         type: ACTION_TYPE.LIQUIDATION,
